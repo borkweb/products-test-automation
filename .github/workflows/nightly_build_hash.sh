5e044bee46e94c5e81034b93ecdfad1d32d4ad68
Luca Tumedei <luca@theaveragedev.com>
2020-03-13 14:29:19 +0100

    build(.github) use sh script for nightly build hash

--- .github/workflows/nightly_build_hash.sh
+++ .github/workflows/nightly_build_hash.sh
@@ -1,27 +1,12 @@
-#!/usr/bin/env php
-<?php
-/**
- * Fires a request to the nightly build service to get the last build timestamp.
- */
+#!/usr/bin/env bash
 
-require_once __DIR__ . '/../../dev/setup/src/utils.php';
-require_once __DIR__ . '/../../dev/setup/src/nightly.php';
+BRANCH="$(printf "%q" "$1")"
 
-if ( 0 === $argc ) {
-	echo "\033[1mGet the last nightly build timestamp for a branch.\033[0m\n";
-	echo "\n";
-	echo "\033[1mUsage:\033[0m\n";
-	echo "\n";
-	echo "\tphp night_build_hash.php <branch> [<licenses_file>]\n";
-	echo "\n";
-	echo "\t\033[32m<branch>\033[0m - The branch to get the nightly build timestamp for.\n";
-	echo "\t\033[32m[<licenses_file>]\033[0m - an optional .env format file that will provide the licenses for each plugin to download, e.g. '.env.licenses'.\n</licenses_file>";
-	exit( 0 );
-}
+# Silent, but show errors.
+TIMESTAMP=$(curl -sSG \
+  --data-urlencode "timestamp=1" \
+  --data-urlencode "branch=${BRANCH}" \
+  --data-urlencode "key=${NIGHTLY_SECRET}" \
+  https://utility.tri.be/nightly.php | jq '.timestamp')
 
-$args = args( [
-	'branch',
-	'licenses_file'
-] );
-
-die( nightly_build_timestamp( $args( 'branch' ), $args( 'licenses_file' ) ) );
+echo "${TIMESTAMP}"
