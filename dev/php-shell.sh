065cf2ada09228def7431d9fe0d58656c58ba234
Luca Tumedei <luca@theaveragedev.com>
2020-03-31 16:53:20 +0200

    fix(dev/php-shell.sh) change color of php shell

--- dev/php-shell.sh
+++ dev/php-shell.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 # Start PHP interactive mode, loading our bootstrap file first.
 php -a \
   -d "auto_prepend_file=${SCRIPT_DIR}/bootstrap.php" \
-  -d "cli.prompt=\e[032mphp \>\e[0m "
+  -d "cli.prompt=\e[1;36mtribe \>\e[0m "
 
