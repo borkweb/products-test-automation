980dc63504d7b3c5217336cff5601c2960854f94
Luca Tumedei <luca@theaveragedev.com>
2020-05-05 15:12:06 +0200

    fix(tric) typo in up command description

--- dev/setup/src/commands/up.php
+++ dev/setup/src/commands/up.php
@@ -5,7 +5,7 @@ namespace Tribe\Test;
 if ( $is_help ) {
 	echo "Starts a container part of the stack.\n";
 	echo PHP_EOL;
-	echo colorize( "signature: <light_cyan>{$cli_name} up <service></service></light_cyan>\n" );
+	echo colorize( "signature: <light_cyan>{$cli_name} up <service></light_cyan>\n" );
 	echo colorize( "example: <light_cyan>{$cli_name} up adminer</light_cyan>" );
 	return;
 }
